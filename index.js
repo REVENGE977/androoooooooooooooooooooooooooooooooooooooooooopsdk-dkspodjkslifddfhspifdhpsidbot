4e0256a73c0e02a7da790b5c586e729a2109ea4d
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-08-08 17:54:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
@@ -40,7 +39,7 @@ bot.on('message', async  message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
+  let prefix = "%"
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
